e8f3446e9a4f84cbebcc623e67178642b362f0e3
Jannis Leidel <jannis@leidel.info>
2017-03-10 14:56:58 +0100

    Add “What’s new?” section to footer

--- atmo/static/js/base.js
+++ atmo/static/js/base.js
@@ -1,9 +1,9 @@
 $(function() {
   AtmoCallbacks = $.Callbacks();
 
-  // load Bootstrap tooltips bubbles
-  var atmoTooltips = function() {
-    $('[data-toggle="tooltip"]').tooltip();
+  // load Bootstrap popovers bubbles
+  var atmoPopovers = function() {
+    $('[data-popover="popover"]').popover();
   };
 
   // load Bootstrap confirmation
@@ -77,10 +77,41 @@ $(function() {
     });
   }
 
-  AtmoCallbacks.add(atmoTooltips);
+  var atmoWhatsNew = function() {
+    // Fill modal with content from link href
+    $('#whatsnew-modal').on('show.bs.modal', function(e) {
+        var link = $(e.relatedTarget);
+        $(this).find('.modal-body').load(link.attr('href'));
+    });
+    var checker = $('#whatsnew-check'),
+        checker_url = checker.attr('data-url');
+    $.get(checker_url).done(function(data) {
+      if (data !== 'ok') {
+        checker.removeClass('hidden');
+        checker.closest('li').removeClass('hidden');
+      }
+    });
+  };
+
+  var atmoTime = function() {
+    var time = $('#time'),
+        utc_now = function() {
+          return moment().utcOffset(0).format('YYYY-MM-DD HH:mm:ss');
+        };
+    var updateTime = function() {
+      time.attr('data-content', utc_now());
+      window.setTimeout(updateTime, 1000);
+    }
+    updateTime();
+  }
+
+  AtmoCallbacks.add(atmoPopovers);
   AtmoCallbacks.add(atmoConfirmations);
   AtmoCallbacks.add(atmoTabs);
   AtmoCallbacks.add(atmoClipboard);
+  AtmoCallbacks.add(atmoWhatsNew);
+  AtmoCallbacks.add(atmoTime);
+
   $(document).ready(function() {
     AtmoCallbacks.fire();
   });
---
+++ atmo/whatsnew/urls.py
@@ -0,0 +1,10 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, you can obtain one at http://mozilla.org/MPL/2.0/.
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^list/$', views.list_whatsnew, name='whatsnew-list'),
+    url(r'^check/$', views.check_whatsnew, name='whatsnew-check'),
+]
